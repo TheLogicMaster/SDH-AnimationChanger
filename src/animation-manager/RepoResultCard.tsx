57b3337530578b25815c718e47bcb71e6dd72324
Stephen Radford <steve228uk@gmail.com>
2022-10-11 15:40:17 +0100

    Fixed focus on panels

--- src/animation-manager/RepoResultCard.tsx
+++ src/animation-manager/RepoResultCard.tsx
@@ -4,16 +4,13 @@ import { Focusable } from 'decky-frontend-lib';
 
 const RepoResultCard: FC<{ result: RepoResult }> = ({ result }) => {
 
-  return (
-    
-    <Focusable
-    focusClassName='gpfocuswithin'
-    className="Panel"
-    style={{
+  return (      
+
+    <div className='Panel' style={{
       margin: 0,
       marginBottom: '15px'
     }}>
-      
+
       <div
       className="gamepadhomewhatsnew_OuterWrapper_3DpEz"
       style={{
@@ -24,7 +21,14 @@ const RepoResultCard: FC<{ result: RepoResult }> = ({ result }) => {
           Animation
         </div>
 
-        <div className="gamepadhomewhatsnew_EventPreviewContainer_1ltOY Panel Focusable">
+        <Focusable
+        focusWithinClassName='gpfocuswithin'
+        className="gamepadhomewhatsnew_EventPreviewContainer_1ltOY Panel"
+        onActivate={() => { console.log('clicked') }}
+        style={{
+          margin: 0,
+          marginBottom: '15px'
+        }}>
           
           <div className="gamepadhomewhatsnew_EventImageWrapper_XLJ9p">
             <img
@@ -32,15 +36,15 @@ const RepoResultCard: FC<{ result: RepoResult }> = ({ result }) => {
               style={{ maxWidth: '100%', height: 'auto', width: 'auto' }}
               className="gamepadhomewhatsnew_EventImage_116GS"/>
             <div className="gamepadhomewhatsnew_Darkener_1n_1X"></div>
-            <div className="gamepadhomewhatsnew_EventSummary_UE_Ms"></div>
+            <div className="gamepadhomewhatsnew_EventSummary_UE_Ms">This is a test</div>
           </div>
 
           <div className="gamepadhomewhatsnew_EventInfo_6TGe7 partnereventdisplay_InLibraryView_3X6U9">
             <div className="partnereventdisplay_EventDetailTimeInfo_3Z41s">
-              <div className="Focusable">
+              <Focusable>
                 <div className="localdateandtime_RightSideTitles_3sPON">Likes</div>
                 <div className="localdateandtime_ShortDateAndTime_4K3Bl">{result.likes}</div>
-              </div>
+              </Focusable>
             </div>
             <div className="gamepadhomewhatsnew_Title_1QLHG">{result.title}</div>
             <div className="gamepadhomewhatsnew_GameIconAndName_1jXSh">
@@ -54,11 +58,11 @@ const RepoResultCard: FC<{ result: RepoResult }> = ({ result }) => {
             </div>
           </div>
 
-        </div>
+        </Focusable>
 
       </div>
 
-    </Focusable>
+    </div>
   );
 
   }
