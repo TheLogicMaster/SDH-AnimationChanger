964d032c22f1906ef8a473fc6c50624861eea07d
Stephen Radford <steve228uk@gmail.com>
2022-10-12 18:32:58 +0100

    Pull out author on modal

--- src/components/RepoResultModal.tsx
+++ src/components/RepoResultModal.tsx
@@ -14,14 +14,19 @@ const RepoResultModal: FC<ModalRootProps & { result: RepoResult }> = ({ result, 
           </video>
         </div>
 
-        <div style={{flex: 1, paddingLeft: '15px'}}>
-          <h2>{result.name}</h2>
+        <div style={{display: 'flex', flex: 1, flexDirection: 'column', paddingLeft: '15px'}}>
+          <div style={{flex: 1}}>
+            <h3 style={{margin: 0}}>{result.name}</h3>
+            <p>Uploaded by {result.author}</p>
+          </div>
+          
           <DialogButton
+          disabled={result.downloaded}
           onClick={() => {
             // 
           }}
           >
-            Download Animation
+            {(result.downloaded) ? 'Downloaded' : 'Download Animation'}
           </DialogButton>
         </div>
       </div>
